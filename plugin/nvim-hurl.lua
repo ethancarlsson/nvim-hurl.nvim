240a254feaf0f966ff47869fe97d7f5feae9f25e
ethancarlsson <ethanmcarlsson@gmail.com>
2023-08-20 15:10:07 +0200

    feat: Create command to yank hurl result

---
+++ plugin/nvim-hurl.lua
@@ -0,0 +1,27 @@
+function HurlYank()
+	local filetype = vim.bo.filetype
+	if filetype ~= 'hurl' then return end
+
+	local filename = vim.api.nvim_buf_get_name(0)
+
+	local handle, err = io.popen('hurl ' .. filename)
+
+	if handle == nil then
+		print('something went wrong while running hurl file')
+		return
+	end
+
+	if not err == nil then
+		vim.fn.setreg('*', err)
+		return
+	end
+
+	local result = handle:read('*all')
+
+	handle:close()
+
+	vim.fn.setreg('*', result)
+end
+
+vim.cmd([[ command! HurlYank lua HurlYank() ]])
+
