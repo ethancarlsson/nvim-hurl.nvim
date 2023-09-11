5b45444f058e465904d97b00e15c45240eaf7f27
ethancarlsson <ethanmcarlsson@gmail.com>
2023-09-11 09:46:43 +0200

    fix: multiline content-type edge case
    This fixes a case where the content tiype gets split onto multiple lines in
    the verbose window. This causes us to miss the content type and not set the
    filetype of the new result window.
    
    This happens when the result gets streamed to stdout in two steps and splits
    the header in two, so we just need to check the first line in case we're on
    the second step of the stream.

--- lua/commands/hurl_run/utilities/hurl_run_service.lua
+++ lua/commands/hurl_run/utilities/hurl_run_service.lua
@@ -20,12 +20,43 @@ local function get_file_type_from_content_type(content_type)
 	return nil
 end
 
+---@param line string
+---@param i integer
+---@return string?
+local function get_file_type(line, i)
+	-- Have to check the first line explicitly in case the headers get split
+	-- accross multiple lines. This usually happens when the response
+	-- is large and can't be returned to stdout/stderr in one go.
+	if i == 1 and (
+	    string_utilities.starts_with(line, 'content-type:', nil, true) ~= nil
+	        or string_utilities.starts_with(line, ': application')
+	    )
+	then
+		return get_file_type_from_content_type(line)
+	end
+
+
+	if not string_utilities.starts_with(line, '<') then
+		return nil
+	end
+
+	local lowered_line = line:lower()
+
+	if string_utilities.starts_with(lowered_line, '< content-type') then
+		return get_file_type_from_content_type(lowered_line)
+	end
+	return nil
+
+end
+
 ---@param verbose_lines string[]
 ---@return string?
 function service.get_file_type_of_response(verbose_lines)
-	for _, s in pairs(verbose_lines) do
-		if string_utilities.starts_with(s:lower(), '< content-type:') then
-			return get_file_type_from_content_type(s)
+	for i, s in ipairs(verbose_lines) do
+		local file_type = get_file_type(s, i)
+
+		if file_type ~= nil then
+			return file_type
 		end
 	end
 
--- lua/commands/hurl_run/utilities/strings.lua
+++ lua/commands/hurl_run/utilities/strings.lua
@@ -1,10 +1,16 @@
-local s = {}
+local M = {}
 
 ---@param str string
 ---@param start string
 ---@return boolean
-function s.starts_with(str, start)
+function M.starts_with(str, start)
 	return str:sub(1, #start) == start
 end
 
-return s
+---@param str string
+---@return string
+function M.trim6(str)
+   return str:match'^()%s*$' and '' or str:match'^%s*(.*%S)'
+end
+
+return M
